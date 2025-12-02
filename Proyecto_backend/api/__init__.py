f04e6ca1e039318f43942ece230d5afbd5f87c61
Damian Cordoba <damianCordoba2@outlook.com>
2025-12-02 07:31:54 -0300

    cambios en el frontend

--- Proyecto_backend/api/__init__.py
+++ Proyecto_backend/api/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask import jsonify
-
+from flask_cors import CORS
  
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def  test():
     return jsonify({"mensaje": "ruta del index"})
